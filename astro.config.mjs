79ebba11ea77728a43cf789fa015f1f80934db58
Yeison Navarro <53413486+LeoNavarro1197@users.noreply.github.com>
2026-04-20 22:11:38 -0500

    add folders

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    }
+  }
+});
